9bbc35de84858c6bdd1d95b4fd8f909b1a446e52
Gabriel Caê Bounous <gccbounous@gmail.com>
2016-04-26 19:24:05 -0300

    tratamento dos dados da corrupcao/ tentaiva de comunicaçao entre os arquivos, vai dar certogit add -A :/

--- dataset/parser/ParserPoliticosBrasilia.py
+++ dataset/parser/ParserPoliticosBrasilia.py
@@ -4,10 +4,9 @@
 from urllib2 import urlopen
 import bs4 as BeautifulSoup
 import csv
-import unicodedata
 import re
-from fuzzywuzzy import fuzz
-import os
+
+todos_politicos = []
 
 def getMinistros():
 	"""
@@ -23,7 +22,7 @@ def getMinistros():
 			nome = []
 			for palavra in linha:
 				if palavra == "-":
-					lista_ministros.append(unicode(" ".join(nome), "utf-8"))
+					lista_ministros.append(unicode(" ".join(nome).upper(), "utf-8"))
 					break;
 				else:
 					nome.append(palavra)
@@ -40,7 +39,7 @@ def getSTF():
 	tagDiv = soup.find("div", id="divImpressao")
 	tagA = tagDiv.find_all("a")
 	for a in tagA:
-		nome = limparLinhaSTF(a.text)
+		nome = limparLinhaSTF(a.text).upper()
 		lista_stf.append(nome)
 	return lista_stf
 
@@ -55,7 +54,7 @@ def getDeputadosFederais():
 	tagsA = soup.find_all("a", title="Detalhes do Deputado")
 	for a in tagsA:
 		tagB = a.find("b")
-		nome = tagB.text
+		nome = tagB.text.upper()
 		lista_deputados.append(nome)
 	return lista_deputados
 
@@ -71,7 +70,7 @@ def getSenadores():
 	for tag in tagTd:
 		tagA = tag.find_all("a")
 		for a in tagA:
-			lista_senadores.append(a.text)
+			lista_senadores.append(a.text.upper())
 	return lista_senadores
 
 def getPoliticosNivelFederal():
@@ -84,9 +83,90 @@ def getPoliticosNivelFederal():
 	lista_politicos  = lista_politicos + getSTF()
 	lista_politicos  = lista_politicos + getDeputadosFederais()
 	lista_politicos  = lista_politicos + getSenadores()
-	lista_politicos.append(unicode("Dilma Rousseff", "utf-8"))
-	lista_politicos.append(unicode("Michel Temer", "utf-8"))
+	lista_politicos.append(unicode("Dilma Rousseff", "utf-8").upper())
+	lista_politicos.append(unicode("Michel Temer", "utf-8").upper())
 	return lista_politicos
+
+def lavaJato():
+	nomes = []
+	with open('./fontes/lavaJato.html','r') as f:
+		html = f.read()
+		soup = BeautifulSoup.BeautifulSoup(html, 'html.parser')
+		alvos = soup.find_all('div',class_='alvo')
+		for alvo in alvos:
+			politico = alvo.strong.string
+			if politicoFederal(politico):
+				nomes.append(politico)
+	return nomes
+
+def panamaPapers():
+	nomes = []
+	pattern = re.compile(".*person.*name.*")
+	csvfile = urlopen('https://raw.githubusercontent.com/amaboura/panama-papers-dataset-2016/master/pt.csv')
+	spamreader = csv.DictReader(csvfile)
+	for line in spamreader:
+		if(pattern.search(line["fieldname"]) is not None):
+			politico = unicode(sanitize(line["text"]), "utf-8")
+			if politicoFederal(politico):
+				nomes.append(politico)
+	return nomes
+
+def odebrecht():
+	nomes = []
+	html = urlopen("http://m.congressoemfoco.uol.com.br/noticias/lista-da-odebrecht-os-politicos-e-seus-respectivos-partidos")
+	soup = BeautifulSoup.BeautifulSoup(html, "html.parser")
+	nomesTr = soup.find_all("td",height="20")
+	for nome in nomesTr[1:]:
+		politico = sanitize(nome.string)
+		if politicoFederal(politico):
+			nomes.append(politico)
+	return nomes
+
+def acusadosCondenados():
+	nomes =[]
+	html = urlopen("http://www.contracorrupcao.org/2013/04/lista-da-corrupcao.html?m=1")
+	soup = BeautifulSoup.BeautifulSoup(html, "html.parser")
+	nomesTr = soup.find_all("td",itemprop="name")
+	for nome in nomesTr:
+		if nome.div.span is not None:
+			politico = sanitize(nome.div.span.string)
+			if politicoFederal(politico):
+				nomes.append(politico)
+	print nomes
+	return nomes
+
+def getPoliticosCorruptos():
+	"""
+	metodo que recupera o nomes dos politicos envolvidos em corrupçao, fazendo uma triagem para termos apenas os politicos da esfera federal
+		-return nomes: um dicionario com listas de nomes relacionadas a cada escandalo
+	"""
+	todos_politicos = getPoliticosNivelFederal()
+	print todos_politicos
+	nomes = dict()
+	print 1
+	nomes["lavaJato"] = lavaJato()
+	print 2
+	nomes["panamaPapers"] = panamaPapers()
+	print 3
+	nomes["odebrecht"] = odebrecht()
+	print 4
+	nomes["acusadosCondenados"] = acusadosCondenados()
+	return nomes
+
+def politicoFederal(politico):
+	"""
+	funcao que verifica se um polico esta na esfera federal ou nao
+		-return politico_ok = True se ele estiver na esfera Federal
+	"""
+	politico = politico.upper().split()
+	politico_ok = False
+	for nome in todos_politicos:
+		nome = nome.split()
+		if nome == politico:
+			print nome," | ",politico
+			politico_ok = True
+			break
+	return politico_ok
 
 def limparLinhaSTF(linha):
 	"""
@@ -100,5 +180,10 @@ def limparLinhaSTF(linha):
 		nome = "".join([linha[1], " ", linha[2], " ", linha[3]])
 	return nome
 
+def sanitize(toto):
+	return toto.split(",")[0]
+
 if __name__ == "__main__":
-	getPoliticosNivelFederal()
+	getPoliticosCorruptos()
+	# todos_politicos = getPoliticosNivelFederal()
+	# acusadosCondenados()
